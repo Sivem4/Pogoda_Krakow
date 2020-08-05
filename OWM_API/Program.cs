60092376085d287a4b9db3e25076bef9e11cd2a2
Sivem4 <69236578+Sivem4@users.noreply.github.com>
2020-08-05 11:26:07 +0200

    test

--- OWM_API/Program.cs
+++ OWM_API/Program.cs
@@ -10,6 +10,7 @@ namespace OWM_API
     {
         static void Main(string[] args)
         {
+            //https://api.openweathermap.org/data/2.5/weather?q=Krak%C3%B3w,PL&appid=fd7fe1df68f992f474b80a734e4d9e17
         }
     }
 }
